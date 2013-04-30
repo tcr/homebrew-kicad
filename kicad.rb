6b504ecff4628d5e8d6301dc38057d93631fb412
Shane Burrell <shane@shaneburrell.com>
2013-04-30 10:31:18 -0400

    Update to 4120

--- kicad.rb
+++ kicad.rb
@@ -16,9 +16,9 @@ end
 
 class Kicad < Formula
   homepage 'https://launchpad.net/kicad'
-  url "http://bazaar.launchpad.net/~kicad-testing-committers/kicad/testing/", :revision => '4107', :using => :bzr
+  url "http://bazaar.launchpad.net/~kicad-testing-committers/kicad/testing/", :revision => '4120', :using => :bzr
   head "http://bazaar.launchpad.net/~kicad-testing-committers/kicad/testing/", :using => :bzr
-  version 'testing-4107'
+  version 'testing-4120'
 
   depends_on 'bazaar'
   depends_on 'cmake' => :build
